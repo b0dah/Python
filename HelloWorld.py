76f51ced042123be61765c10534fb76907976c69
b0dah <ries8898@gmail.com>
2020-02-18 00:25:53 +0300

    second ind done, learn recursion, man

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,10 @@
 print('hola')
+
+def func(i):
+	if i == 2:
+		return True
+	else:
+		func(i+1)
+		return False
+	
+print(func(0))
--- SecondIndividual.py
+++ SecondIndividual.py
@@ -1,40 +1,54 @@
-# dict (eachLeaf: parent)
-
 def isAncestor(dict, supposedAncestor, element):
-	if not element in dict: 
+	print("supsd =", supposedAncestor, "cur el =", element)
+	if not element in dict:
+		print("fl")
 		return False
 	elif dict[element] == supposedAncestor:
+		print("ype")
 		return True
 	else:
-		isAncestor(dict, dict[element], supposedAncestor)
+		return isAncestor(dict, supposedAncestor, dict[element])
+	
+	
 		
 #### Main
 numberOfEls = int(input("Eneter the numb of Els ... "))
 familyTreeDict = {}
 
+# SOURCE TREE
 print('Enter the source tree')
 for i in range(numberOfEls-1):
 	current = input("Enter pair: (El - Parent):\n").split()
 	familyTreeDict[current[0]] = current[1]
-	
-#numberOfPairsToDefine = int(input("Eneter the number Of Pairs To Define ...\n"))
-#
-#pairsToDefine = []
-#print('Enter the pairs to define')
-#for i in range(numberOfPairsToDefine):
-#	current = tuple(input("nter pair: (El - Parent):\n"))
-#	pairsToDefine.add(current)
 
+print(familyTreeDict)
 
+# DEFINING
+numberOfPairsToDefine = int(input("Eneter the number Of Pairs To Define ...\n"))
+
+pairsToDefine = []
+print('Enter the pairs to define')
+for i in range(numberOfPairsToDefine):
+	current = tuple(input("nter pair: (El - Parent):\n").split())
+	pairsToDefine.append(current)
 	
-for key, value in familyTreeDict.items():
-	if isAncestor(familyTreeDict, key, value):
-		print(key, "-", value, "	1")
-	elif isAncestor(familyTreeDict, value, key):
-		print(key, "-", value, "	2")
+# SOLVING
+for pair in pairsToDefine:
+	if isAncestor(familyTreeDict, pair[0], pair[1]):
+		print(pair[0], "-", pair[1], "	1")
+	elif isAncestor(familyTreeDict, pair[1], pair[0]):
+		print(pair[0], "-", pair[1], "	2")
 	else:
-		print(key, "-", value, "	0")
-
+		print(pair[0], "-", pair[1], "	0")
+	
+#for key, value in familyTreeDict.items():
+#	if isAncestor(familyTreeDict, key, value):
+#		print(key, "-", value, "	1")
+#	elif isAncestor(familyTreeDict, value, key):
+#		print(key, "-", value, "	2")
+#	else:
+#		print(key, "-", value, "	0")
+#
 
 	
 
